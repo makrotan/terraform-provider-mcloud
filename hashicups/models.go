2136b65b3165d6eb1944ac7f6a3c43c219479009
Martin Klapproth <martin.klapproth@makrotan.com>
2022-02-11 11:59:17 +0100

    WIP: sshkey

--- hashicups/models.go
+++ hashicups/models.go
@@ -30,3 +30,9 @@ type Ingredient struct {
 	Quantity int    `json:"quantity"`
 	Unit     string `json:"unit"`
 }
+
+type SshKey struct {
+	Name       string `json:"name"`
+	PublicKey  string `json:"public_key"`
+	PrivateKey string `json:"private_key"`
+}
